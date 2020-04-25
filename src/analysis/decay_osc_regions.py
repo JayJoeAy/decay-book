ef26bab83f72434e948c1eae0b27b937fc0a6e52
ErfanHamdi <erfan.hamdi@gmail.com>
2020-04-25 16:49:17 +0430

    Added the decay modeling function and used matplotlib instead of scitools for plotting the contours.

--- src/analysis/decay_osc_regions.py
+++ src/analysis/decay_osc_regions.py
@@ -1,6 +1,37 @@
-from decay_mod import solver
 import numpy as np
-import scitools.std as st
+import matplotlib.pyplot as plt
+
+
+
+def solver(I, a, T, dt, theta):
+    """
+    solving for u' = -au
+
+    Parameters
+    ----------
+    I : Initial Condition
+    a : decay rate
+    T : Time
+    dt : Time step
+    theta : Method Model, theta = 1 FE, theta = 0 BE, theta = 0.5 CN
+
+    Returns
+    -------
+    u : desired function
+    t : t to the desired function
+
+    """
+    dt = float(dt)
+    Nt = int(round(T/dt))
+    T  = Nt*dt
+    u = np.zeros(Nt+1)
+    t = np.linspace(0, T, Nt+1)
+    
+    u[0] = I
+    for n in range(0,Nt):
+        u[n+1] = (1-(1-theta)*a*dt)/(1+theta*dt*a)*u[n]
+    return u, t
+
 
 def non_physical_behavior(I, a, T, dt, theta):
     """
@@ -21,13 +52,13 @@ def non_physical_behavior(I, a, T, dt, theta):
                     correct_qualitative_behavior = False
                     break  # Jump out of loop
             B[i,j] = float(correct_qualitative_behavior)
-    a_, dt_ = st.ndgrid(a, dt)  # make mesh of a and dt values
-    st.contour(a_, dt_, B, 1)
-    st.grid('on')
-    st.title('theta=%g' % theta)
-    st.xlabel('a'); st.ylabel('dt')
-    st.savefig('osc_region_theta_%s.png' % theta)
-    st.savefig('osc_region_theta_%s.pdf' % theta)
+    a_, dt_ = np.meshgrid(a, dt)  # make mesh of a and dt values
+    plt.contour(a_, dt_, B, levels = [1])
+    plt.grid('on')
+    plt.title('theta=%g' % theta)
+    plt.xlabel('a'); plt.ylabel('dt')
+    plt.savefig('osc_region_theta_%s.png' % theta)
+    plt.savefig('osc_region_theta_%s.pdf' % theta)
 
 non_physical_behavior(
     I=1,
